1f71143912c8895be5fb623945a2616ded64e411
Carlos Chew <33320933+CarlosChew@users.noreply.github.com>
2018-03-01 15:11:35 -0600

    New features

--- OperativeSystem.py
+++ OperativeSystem.py
@@ -1,12 +1,13 @@
 # -*- coding: cp1252 -*-
-         #UNIVERSIDAD DEL VALLE DE GUATEMALA
-         #HOJA DE TRABAJO NO. 5
 
-            #ALGORITMOS Y ESTRUCTURA DE DATOS
+#UNIVERSIDAD DEL VALLE DE GUATEMALA
+#HOJA DE TRABAJO NO. 5
+
+#ALGORITMOS Y ESTRUCTURA DE DATOS
             
 
-                #ALEXANDER TRUJILLO (17189)
-                #CARLOS CHEW (17507)
+#ALEXANDER TRUJILLO (17189)
+#CARLOS CHEW (17507)
 
 
 
@@ -14,6 +15,9 @@
 import random
 import simpy
 
+print ("")
+print ("---OPERATIVE SYSTEM SIMULATION---")
+print ("")
 #FUNCTION DEFINES AS SIMULATION, WHICH CONTAINS PARAMETERS 
 def simulation(processName,x,waiting,CPU):
 
@@ -26,18 +30,19 @@ def simulation(processName,x,waiting,CPU):
  
     ready = x.now
 
-    running = random.randint(1, 10)
-    print ('%s Process %f needs %d ammount of RAM' % (processName,ready,running))
+
+    running = random.randint(1, 200)
+    print ('%s /Process %f needs %d ammount of RAM' % (processName,ready,running))
     
 
     with CPU.request() as CPUturn:
         yield CPUturn     
         yield x.timeout(running) 
-        print ('%s Finish the process on %f' % (processName, x.now))
+        print ('%s /Finish the process on %f' % (processName, x.now))
         
         
     totalTime = x.now - ready
-    print ('%s It takes %f' % (processName, totalTime))
+    print ('%s /It takes %f' % (processName, totalTime))
     wholeProcess = wholeProcess + totalTime
            
 
@@ -47,9 +52,13 @@ x = simpy.Environment()
 CPU = simpy.Resource(x,capacity = 1)
 random.seed(10) 
 wholeProcess = 0
-for i in range(5):
-    x.process(simulation('Process %d'%i,x,random.expovariate(1.0/10),CPU))
+for i in range(25):#RANGE 
+    x.process(simulation('Process %d'%i,x,random.expovariate(1.0/10),CPU)) 
 
+#IT RUNS THE SIMULATION UNTIL 50 VALULES 
 x.run(until=50)  
 
-print ("The average time of the process is: ", wholeProcess/5.0)
+print ("")
+print ("")
+print ("---AVERAGE PROCESS---")
+print ("The average time of the process is: ", wholeProcess/25.0)
